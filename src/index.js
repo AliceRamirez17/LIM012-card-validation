7184a3cb5b01bb8ec70d23c1b76563ffce3a2788
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-02-13 09:47:20 -0500

    alicemarcelaramirez@gmail.com

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+function verificar(){
+    const numeroTarjeta = document.getElementById('numero').value
+    const invertido = numeroTarjeta.split('').reverse()
+    
+
+    const multiplicacion = []
+    multiplicacion[0] = invertido[1];
+    multiplicacion[1] = invertido[3];
+    multiplicacion[2] = invertido[5];
+    multiplicacion[3] = invertido[7];
+    multiplicacion[4] = invertido[9];
+    multiplicacion[5] = invertido[11];
+    multiplicacion[6] = invertido[13];
+    multiplicacion[7] = invertido[15];
+
+    console.log(multiplicacion)
+
+
+}
+
+
+
+    //.reduce(function(r, n) { return r + parseInt(n) }, 0));
+    //document.getElementById('resultado').innerHTML = split('')
+    //console.log(typeof(resultado))
+
+    var btn = document.getElementById('button');
+    btn.addEventListener('click', verificar, false);
+
+         
+    // (parseInt(numeroTarjeta))
