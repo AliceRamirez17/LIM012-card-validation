308252d1ac3ad00630aece6a5e84b87a29808d3c
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-02-16 09:49:39 -0500

    alicemarcelaramirez@gmail.com

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ import validator from './validator.js';
 
 console.log(validator);
 
-const numeroTarjeta = document.getElementById('numero');
-const newArray = [];
+function obtenerNumero(){
+    // Obtener el valor el input
+    const numeroTarjeta = document.getElementById('numero').value;
+    
+    // invertir el texto
+    const size = numeroTarjeta.length;
+    const lastPosition = size-1;
 
-const cadena = function submit(){
-    newArray.push(numeroTarjeta.value);
-    var convertirCadena = newArray.toString().split("");
-    console.log(convertirCadena);
+    let textoInvertido = '';
+
+    for(let i=lastPosition; i>=0; i--){
+        textoInvertido = textoInvertido + numeroTarjeta[i];
+    }
+
+    // convertir el texto a un número y convertirlo en un array
+    const NewArray = Array.from(textoInvertido).map(Number);
+
+    // 
+
+    console.log(NewArray);
+
 }
 
-
-    const btn = document.getElementById('button');
-    btn.addEventListener('click', cadena, false);
+const btn = document.getElementById('button');
+btn.addEventListener('click', obtenerNumero, false);
