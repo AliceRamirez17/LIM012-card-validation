bfecab48da4dc296b1342722334aef250d6f98f9
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-02-17 13:12:16 -0500

    alicemarcelaramirez@gmail.com

--- src/index.js
+++ src/index.js
@@ -19,10 +19,44 @@ function obtenerNumero(){
     // convertir el texto a un número y convertirlo en un array
     const NewArray = Array.from(textoInvertido).map(Number);
 
-    // 
+    console.log('Array de número invertido',NewArray);
 
-    console.log(NewArray);
+    // seleccionar las posiciones impares
+    let listaImpares = [];
 
+    for(let i=0; i<NewArray.length; i++){
+        if(i%2!==0){
+            listaImpares.push(NewArray[i]*2);
+        }else{
+            listaImpares.push(NewArray[i]*1);
+        }
+    }
+
+    console.log('lista posiciones impares *2',listaImpares);
+
+    //sumar los digitos mayores a 10
+    let lista2 = [];
+
+    for(let i=0; i<listaImpares.length; i++){
+        if(listaImpares[i]>=10){
+        lista2.push((listaImpares[i]-10)+1);
+        }else{
+        lista2.push(listaImpares[i]*1);
+        }
+    }
+
+    console.log('digitos de números >10',lista2);
+    
+    const total = lista2.reduce((sum, current) => sum + current, 0);
+    console.log(total);
+
+    let verificado = false;
+    if(total%10===0){
+        return true
+    }
+
+    console.log(verificado);
+    
 }
 
 const btn = document.getElementById('button');
